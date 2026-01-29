3a7ca844a0c143d35e6f08abba4731a5d2b76850
Marty450 <kavrakovmartin3@gmail.com>
2026-01-29 13:10:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.write("Do you like Python?")
 answer = st.text_input()
-if answer==yes:
+if answer=="yes":
   st.write("Great!")
-elif answer==no:
+elif answer=="no":
   st.write("you will like it")
